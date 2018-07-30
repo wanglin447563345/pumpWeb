41ae0ae2262300c037fd986455f57073e25395ee
WALL.E <447563345@qq.com>
2018-07-30 18:25:50 +0800

    解决路由不能动态加载

--- src/routes/Register/index.js
+++ src/routes/Register/index.js
@@ -3,7 +3,7 @@ import { dynamicWrapper, createRoute } from '@/utils/core';
 const routesConfig = (app) => ({
     path: '/pump/user/register',
     title: '用户注册',
-    component: dynamicWrapper(app, [import('./model')], import('./components')),
+    component: dynamicWrapper(app, [import('./model')], ()=>import('./components')),
 });
 
 export default (app) => createRoute(app, routesConfig);
