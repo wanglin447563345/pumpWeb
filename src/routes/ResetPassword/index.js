8406849d726e91b8a75ad4b537f1f2b02dbe1c37
WALL.E <447563345@qq.com>
2018-07-30 18:26:47 +0800

    解决路由不能动态加载

--- src/routes/ResetPassword/index.js
+++ src/routes/ResetPassword/index.js
@@ -3,7 +3,7 @@ import { dynamicWrapper, createRoute } from '@/utils/core';
 const routesConfig = (app) => ({
     path: '/pump/user/reset',
     title: '重置密码',
-    component: dynamicWrapper(app, [import('./model')], import('./components')),
+    component: dynamicWrapper(app, [import('./model')], ()=>import('./components')),
 });
 
 export default (app) => createRoute(app, routesConfig);
