d1db0706c068e17352c4f66e5f7920d480332b3a
WALL.E <447563345@qq.com>
2018-07-30 18:23:16 +0800

    解决路由不能动态加载

--- src/routes/List_Add/List/index.js
+++ src/routes/List_Add/List/index.js
@@ -3,7 +3,7 @@ import {dynamicWrapper, createRoute} from '@/utils/core';
 const routesConfig = (app) => ({
     path: '/pump/web/control/list',
     title: '列表',
-    component: dynamicWrapper(app, [import('./model')], import('./components'))
+    component: dynamicWrapper(app, [import('./model')], ()=>import('./components'))
 });
 
 export default (app) => createRoute(app, routesConfig);
